5cbcbbbfb6c5ed18ce495c7e65f43bcff70b6de3
maressyl <maressyl@gmail.com>
2015-05-19 12:28:46 +0200

    Fixing segOverlap()

--- Rgb.Rcheck/00_pkg_src/Rgb/R/sizetrack.R
+++
@@ -1,7 +0,0 @@
-# Memory and time efficient subsetting of chromosomal tracks (chrom, start, end) (count hits only)
-# Author : Sylvain Mareschal <maressyl@gmail.com>
-# License : GPL3 http://www.gnu.org/licenses/gpl.html
-
-sizetrack = function(...) {
-	.External("track", PACKAGE="Rgb", mode="size", ...)
-}
